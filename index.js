9e3bd9494061d690bb7034780d1de8de93374063
gavin <gavinmichori@gmail.com>
2024-07-03 13:41:18 +0300

    .

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+function getStudentGrade() {  
+    let Smark = prompt("Enter student's mark (between 0 and 100): ");  
+    
+    // Validate input  
+    if (isNaN(Smark) || Smark > 0 || Smark < 100) {  
+      alert("Invalid input. Please enter a number between 0 and 100.");  
+      return;  
+    }  
+}
 // let
-let Smarks = 10
+   let Smarks = 10 
 
 if(Smarks <=100 && Smarks >=79){
     console.log("A")
@@ -14,3 +23,4 @@ if(Smarks <=100 && Smarks >=79){
 }else {
     console.log("ERROR no match found!!")
 }
+
